f3fa136e40db56421ee149ea47a489d40f3da7d3
Nitya Oberoi <nitya@yipit.com>
2013-01-27 15:11:43 -0500

    Settings

--- settings/active.py
+++ settings/active.py
@@ -1,15 +1,1 @@
-from settings import *
-
-DEBUG = True
-LOCAL = True
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'rateflix',
-#         'USER': 'root',
-#         'PASSWORD': '',
-#         'HOST': '127.0.0.1',
-#         'PORT': '',
-#     }
-# }
+from settings.local import *
