e4b8092272e6ba40337c36a3e37e589dccf0c822
Nitya Oberoi <nitya@yipit.com>
2013-01-27 16:18:08 -0500

    Correct user

--- settings/production.py
+++ settings/production.py
@@ -7,7 +7,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'heroku_3000ec83c1561da',
-        'USER': '//b3fc7ab06a9e40',
+        'USER': 'b3fc7ab06a9e40',
         'PASSWORD': '5f5ced39',
         'HOST': 'us-cdbr-east-03.cleardb.com',
         'PORT': '3306',
