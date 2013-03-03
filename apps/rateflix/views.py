d99016e315c6626645ac8eaa576df37ed29be723
Nitya Oberoi <nitya@yipit.com>
2013-03-02 22:32:30 -0500

    Templates

--- apps/rateflix/views.py
+++ apps/rateflix/views.py
@@ -1,6 +1,38 @@
 from django.views.generic.base import TemplateView
 
+from rateflix.models import Movie, Genre
+
 
 class LandingView(TemplateView):
 
-    template_name = 'site_base.html'
+    template_name = 'movies.html'
+
+    def get(self, request, *args, **kwargs):
+        genre = request.GET.get('genre')
+        if genre:
+            kwargs['genre__name'] = genre
+        return self.render_to_response(self.get_context_data(**kwargs))
+
+    def get_context_data(self, **kwargs):
+        data = {
+            'genres': Genre.objects.exclude(movie=None).order_by('name'),
+            'movies': self.get_movies(**kwargs),
+            'types': ['TV show', 'movie', 'anything']
+        }
+        return data
+
+    def get_movies(self, **filters):
+        movies = (Movie
+                .objects
+                .filter(**filters)
+                .rated()
+                .order_by('-rating')[:10])
+        return [movie.context_manager.get_context_for_popover()
+                    for movie in movies]
+
+
+class AutocompleteView(TemplateView):
+
+    def get(self, request, *args, **kwargs):
+        import ipdb; ipdb.set_trace();
+        return super(AutocompleteView, self).get(request, *args, **kwargs)
--- urls.py
+++ urls.py
@@ -1,12 +1,18 @@
+from django.conf import settings
 from django.conf.urls import patterns, url, include
+from django.conf.urls.static import static
 
 from django.contrib import admin
 admin.autodiscover()
 
-from rateflix.views import LandingView
+from rateflix.views import LandingView, AutocompleteView
 
 urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', LandingView.as_view(), name='home'),
+    url(r'^ajax/autocomplete/$', AutocompleteView.as_view(), name='home'),
 )
+
+if settings.LOCAL:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
